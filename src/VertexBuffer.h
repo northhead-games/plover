453530d994f3371b45dfbe25893aa174a286d8d6
Oliver Frank <oliverfrank321@gmail.com>
2023-02-23 00:17:29 +0000

    tweak: change color

--- src/VertexBuffer.h
+++ src/VertexBuffer.h
@@ -41,7 +41,7 @@ namespace Plover {
 		VkBuffer buffer;
 
 		const std::vector<Vertex> vertices = {
-			{{ 0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+			{{ 0.0f, -0.5f}, {1.0f, 1.0f, 1.0f}},
 			{{ 0.5f,  0.5f}, {0.0f, 1.0f, 0.0f}},
 			{{-0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}}
 		};
